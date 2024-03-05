9246384849cba35b948e06dbc38779604412f977
etnicotinate <etnicotinate@gmail.com>
2024-03-05 11:52:37 +0800

    anneal-change, born-1k

---
+++ 7_anneal_mech/change/calcs/multi_run.sh
@@ -0,0 +1,30 @@
+# nohup ./multi_run.sh > nohup.out &
+i=0
+for file in *
+do  
+    if test -d $file
+    then
+        dir=$file
+        cd $dir
+        i=$((i+1))
+        echo "task $i, in dir: $dir" &
+        ./run.sh &
+        sleep 0.5
+        # keep 6 tasks running at the same time (56 cores > 8*1*6  )
+        if [ $i -eq 99 ]
+        then
+            wait
+            echo
+            break
+        fi
+        if [ $((i%6)) -eq 0 ]
+        then
+            wait
+            echo
+        fi
+        cd ..
+    fi
+done
+wait
+echo "All tasks are done."
+
