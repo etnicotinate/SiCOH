304f93e0c9377f6b12b26dbf52efe40ecd89e071
etnicotinate <etnicotinate@gmail.com>
2024-04-05 14:27:12 +0800

    4.5

--- 6_SiOCH_mech/deform/deform_box/calcs/get_data.sh
+++ 6_SiOCH_mech/deform/deform_box/calcs/get_data.sh
@@ -2,24 +2,35 @@ log="data.log"
 rm $log
 touch $log
 
-printf "# %-24s%-16s%-12s%+10s\n" "Structure" "Density" "Youngs(GPa)" "Stability" >> ./$log
+printf "# %-24s%-16s%-12s%+10s\n" "Structure" "Density" "Youngs(GPa)" "Confidence interval(GPa)" "Stability" >> ./$log
 for file in *
     do  
         if test -d $file
         then
             dir=$file
             cd $dir
+            # Structure column
             printf "%-25s" $dir >> ../$log
+            # Density column
             awk '/Density/{getline; printf "%-16s", $5; exit;}' out_sicoh.log >> ../$log
+
+            # compute modulus and error
             if [ ! -f "modulus.log" ]; then
                 python ./compute.py > modulus.log
                 # python ./tensor2modulus.py > modulus.log
             fi
+            # python ./compute.py > modulus.log
+
+            # Young's modulus column
             awk '/Young/{printf "%-16s", $3}' modulus.log >> ../$log
+            # Confidence interval column
+            awk '/Confidence/{printf "%-16s", $5}' modulus.log >> ../$log
+
+            # Stability column
             if grep -q "unstable" modulus.log; then
                 printf "%+10s" |echo "u" >> ../$log
             else
-                printf "%+10s" |echo " " >> ../$log
+                printf "%+10s" |echo "s" >> ../$log
             fi
             # echo >> ../$log
             echo "Data for $dir has been extracted."
